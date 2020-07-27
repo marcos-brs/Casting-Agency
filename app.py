e5e91301d6033eb778ebc44b3fb133008acd516c
Marcos Santana <brmarcossantanabr@gmail.com>
2020-07-27 20:25:23 -0300

    feat: list all actors

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Actors
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
     CORS(app)
     setup_db(app)
+
+    @app.route('/actors', methods=['GET'])
+    def list_actors():
+        actors = Actors.query.all()
+
+        return jsonify({
+            'success': True,
+            'actors': actors
+        })
 
     return app
 
