6adf1a2f1def1d1ea9f972416a1d97061ac36faf
Marcos Santana <brmarcossantanabr@gmail.com>
2020-07-27 20:43:01 -0300

    feat: create an actor

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from models import setup_db, Actors, Movies
-
+import json
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -27,6 +27,37 @@ def create_app(test_config=None):
         return jsonify({
             'success': True,
             'movies': formatted_movies
+        })
+
+    @app.route('/actors', methods=['POST'])
+    def create_actor():
+        body = request.get_json()
+
+        if body is None:
+            abort(422)
+
+        required_params = ['name', 'age', 'gender']
+
+        for param in required_params:
+            if param not in body:
+                abort(422)
+
+        name = body.get('name')
+        age = body.get('age')
+        gender = body.get('gender')
+
+        if gender != "female" and gender != "male":
+            abort(22)
+
+        try:
+            new_actor = Actors(name=name, age=age, gender=gender)
+            new_actor.insert()
+        except:
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'created': new_actor.id
         })
 
     return app
