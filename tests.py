bc19f345c2a020c03da114f02f4128c676beff4e
Marcos Santana <brmarcossantanabr@gmail.com>
2020-07-28 09:40:59 -0300

    Test: post actors and movies for assistant role

--- tests.py
+++ tests.py
@@ -65,6 +65,28 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    def test_assistant_post_actors(self):
+        headers = {
+            'Authorization': 'Bearer {}'.format(TOKEN_ASSISTANT)
+        }
+        res = self.client().post('/actors', headers=headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['code'], 'unauthorized')
+        self.assertEqual(data['description'], 'Permission not found.')
+
+    def test_assistant_post_movies(self):
+        headers = {
+            'Authorization': 'Bearer {}'.format(TOKEN_ASSISTANT)
+        }
+        res = self.client().post('/movies', headers=headers)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['code'], 'unauthorized')
+        self.assertEqual(data['description'], 'Permission not found.')
+
 
 if __name__ == '__main__':
     unittest.main()
