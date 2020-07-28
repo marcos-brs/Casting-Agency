3e5d7f1cee7c583700c4e9da225e86a793a840cf
Marcos Santana <brmarcossantanabr@gmail.com>
2020-07-28 11:29:10 -0300

    Update app variable

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ def create_app(test_config=None):
     return app
 
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='localhost', port=80, debug=True)
+    app.run(host='localhost', port=80, debug=True)
